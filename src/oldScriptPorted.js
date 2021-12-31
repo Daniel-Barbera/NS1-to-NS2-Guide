7e98785d1abd6a4a29349688763ea6956a54ba3d
Daniel Barbera <87785383+xRalic@users.noreply.github.com>
2021-12-31 04:12:50 +0100

    Add source code and images

---
+++ src/oldScriptPorted.js
@@ -0,0 +1,31 @@
+/** @param {NS} ns **/
+export async function main(ns) {
+	var servers0Port = ["n00dles",
+						"sigma-cosmetics",
+						"joesguns",
+						"nectar-net",
+						"hong-fang-tea",
+						"harakiri-sushi"];
+	var servers1Port = ["neo-net",
+						"zer0",
+						"max-hardware",
+						"iron-gym"];
+	for (var i = 0; i < servers0Port.length; ++i) {
+		var serv = servers0Port[i];
+
+		await ns.scp("early-hack-template.script", serv);
+		ns.nuke(serv);
+		ns.exec("early-hack-template.script", serv, 6);
+	}
+	while (!ns.fileExists("BruteSSH.exe")) {
+		await ns.sleep(60000);
+	}
+	for (var i = 0; i < servers1Port.length; ++i) {
+		var serv = servers1Port[i];
+
+		await ns.scp("early-hack-template.script", serv);
+		ns.brutessh(serv);
+		ns.nuke(serv);
+		ns.exec("early-hack-template.script", serv, 12);
+	}
+}
